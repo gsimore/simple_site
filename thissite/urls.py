c1be075c9f9f66ffd0580fba9b8fca3286a99732
Gabrielle Simard-Moore <gsimardmoore@gmail.com>
2017-05-31 16:49:09 -0700

    Begin django templating to shop-index.html, with items displaying title, image and price, using implementation of Bootstrap shop homepage template.

--- thissite/urls.py
+++ thissite/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
     url(r'', include('shop.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
